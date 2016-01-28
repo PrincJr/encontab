05a1cbf481059adc33c1a84f9f1fe5e4601e21e9
Gohan <gohan@domain.net>
2016-01-28 16:16:35 -0200

    Inlcuindo projeto. Incluindo classes com as contantes que definem as TAGS do arquivo XML.

---
+++ src/encontab/consts/NfceEmitente.java
@@ -0,0 +1,30 @@
+package encontab.consts;
+
+import java.lang.String;
+
+public class NfceEmitente {
+	public static final String ID_EMITENTE_TAG = "Emit";
+	public static final String CNPJ_TAG = "CNPJ";
+	public static final String CPF_REMETENTE_TAG = "CPF";
+	public static final String RAZAO_SOCIAL_EMITENTE_TAG = "xNome";
+	public static final String NOME_FANTASIA_TAG = "xFant";
+	public static final String ENDERECO_EMITENTE_TAG = "enderEmit";
+	public static final String LOGRADOURO_TAG = "xLgr";
+	public static final String NUMERO_TAG = "nro";
+	public static final String COMPLEMENTO_TAG = "xCpl";
+	public static final String BAIRRO_TAG = "xBairro";
+																//Código do Município
+	public static final String COD_MUNICIPIO_TAG = "cMun";
+	
+	public static final String NOME_MUNICIPIO_TAG = "xMun";
+	
+	public static final String UF_TAG = "UF";
+	
+	public static final String CEP_TAG = "CEP";
+																//Nome do País Brasil ou BRASIL
+	public static final String NOME_PAIS_TAG = "xPais";
+																//Código do País Brasil = 1058
+	public static final String COD_PAIS_TAG = "cPais";
+	
+	public static final String FONE_TAG = "fone";
+}
---
+++ src/encontab/consts/NfceInfo.java
@@ -0,0 +1,39 @@
+package encontab.consts;
+
+public class NfceInfo {
+	public static final String RootTag = "NFe";
+	public static final String VersaoLayoutAttr = "versao";
+	public static final String IdTagAssinadaAttr = "id";
+	public static final String InfNFeTag = "InfNFe";
+	public static final String IdentificationTag = "ide";
+	public static final String CodUnidadeFederativaEmitenteTag = "cUF";
+	public static final String CodNumChaveAcessoTag = "nNF";
+	public static final String NaturezaOperacaoTag = "natOp";
+	public static final String IndicadorFormaPagamentoTag = "indPag";
+	public static final String ModeloDocumentoTag = "mod";
+	public static final String SerieDocumentoTag = "serie";
+	public static final String NumDocumentoFiscalTag = "nNF";
+	public static final String DataHoraEmissaoTag = "dhEmi";
+	public static final String DataHoraEntradaSaidaTag = "dhSaiEnt";
+	public static final String TipoOperacaoTag = "tpNF";
+	public static final String IdentificaoDestinoTag = "idDest";
+	public static final String CodNunicipioFatoGeradorTag = "cMunFG";
+	public static final String TipoImpressaoDanfeTag = "tpImp";
+	public static final String TipoEmissaoTag = "tpEmis";
+	public static final String DigitoVerificadorChaveAcessoTag = "cDV";
+	public static final String TipoAmbienteTag = "tpAmb";
+	public static final String FinalidadeNotaFiscalTag = "finNFe";
+			//Indica operação com Consumidor final
+	public static final String IndOperacaoConsumidorFinalTag = "indFinal";
+			//Indicador de presença do comprador no estabelecimento comercial no momento da operação
+	public static final String IndConsumidorPresenteTag = "indPress";
+																	//Processo de emissão da NF-e
+	public static final String ProcessoEmissaoTag = "procEmi";
+																	//Versão do Processo de emissão da NF-e
+	public static final String VersaoProcessoEmissaoTag = "verProc";
+																	//Sequência XML (opcional)
+	public static final String SequenciaXmlTag = "-x-";
+	public static final String DataHoraContingenciaTag = "dhCont";
+	public static final String JustificativaContingenciaTag = "xJust";
+	
+}
